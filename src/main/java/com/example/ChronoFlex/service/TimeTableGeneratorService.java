d5be867c5c2c6a100fc8735955dc196fae548cef
SandeepHemba <sandeephemba@gmail.com>
2026-04-15 23:11:35 +0530

    Fix import case for FacultyAvailabilityRepository

--- src/main/java/com/example/ChronoFlex/service/TimeTableGeneratorService.java
+++ src/main/java/com/example/ChronoFlex/service/TimeTableGeneratorService.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.example.chronoflex.repository.FacultyAvailabilityRepository;
+import com.example.ChronoFlex.repository.FacultyAvailabilityRepository;
 
 import java.time.LocalTime;
 import java.util.*;
